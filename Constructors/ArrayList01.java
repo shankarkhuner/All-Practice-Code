0d02022a68217d4e07efb4d081501b1654d64ec8
Shankar Khuner <125340488+shankarkhuner@users.noreply.github.com>
2023-06-21 22:31:38 +0530

    Practice Array

---
+++ Constructors/ArrayList01.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+
+public class ArrayList01
+{
+	public static void main(String[] args) 
+	{
+		ArrayList a = new ArrayList();
+		a.add(100);
+		a.add(200.25f);
+		a.add("X");
+		System.out.println(a);
+		System.out.println(a.toString());
+	}
+}
